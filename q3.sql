630620c7f4a656e265d69d4d0c31c51e9f2310f9
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2022-05-19 18:29:19 -0600

    Update q3.sql
    Question 3 solution

--- q3.sql
+++ q3.sql
@@ -11,11 +11,25 @@ SET @v7 = 'EE';
 SET @v8 = 'MAT';
 
 -- 3. List the names of students who have taken course v4 (crsCode).
+--  ************************ORIGINAL *************************
+
+SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+
 
 
 --  ********************************   ANSWER   ***********************************************
 -- This has correlated subqueries, use derived tables instead?
 -- Use Union all instead of IN?
+
+-- This one runs correctly with the correct students.
+-- This uses a CTE and correct type of JOIN to arrive at the same answer
 --  ********************************   ANSWER   ***********************************************
 
-SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+
+WITH studIds AS
+(
+  SELECT studId FROM Transcript WHERE crsCode = @v4
+)
+
+SELECT name FROM Student 
+	RIGHT OUTER JOIN studIds ON id=studId;
